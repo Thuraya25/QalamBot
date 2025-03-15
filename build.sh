110d07b961c8db8a7776088537ad8b1f982056c1
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-15 18:07:47 +0300

    Removed sudo from build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Java (required for language_tool_python)
-sudo apt-get update
-sudo apt-get install -y default-jre
+apt-get update
+apt-get install -y default-jre
 
 # Install Python dependencies
 pip install -r requirements.txt
