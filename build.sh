3db3ae7d81e5e7916b7643cbb7c1a6f1538ce834
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-15 20:46:50 +0300

    Fixed missing tput by installing ncurses-utils

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Install Java 11 (if not already installed)
+# Install necessary utilities like tput
 sudo apt update
+sudo apt install -y ncurses-utils
+
+# Install Java 11
 sudo apt install -y openjdk-11-jdk
 
 # Set JAVA_HOME
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Check if Java is installed
+# Check Java version
 java -version
 
 # Install Python dependencies
