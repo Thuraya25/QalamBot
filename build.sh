889f440d6527f87177f2ff2239d39be86cbd752b
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-15 20:28:17 +0300

    Updated build.sh to use Java 11

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Install Java 23 (required for language_tool_python)
+# Install Java 11 (required for language_tool_python)
 apt-get update
-apt-get install -y openjdk-23-jre
+apt-get install -y openjdk-11-jre
+
+# Set JAVA_HOME environment variable
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
 # Install Python dependencies
 pip install -r requirements.txt
