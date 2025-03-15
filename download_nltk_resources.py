2f6ee32c037d5eae503a36646caeedceefc3e665
Thuraya25 <Thuraya.Althumali@gmail.com>
2025-03-15 21:47:28 +0300

    Add files via upload

---
+++ download_nltk_resources.py
@@ -0,0 +1,18 @@
+import nltk
+
+# Download the necessary NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('words')
+nltk.download('omw-1.4')
+nltk.download('wordnet')
+
+import os
+
+# Set NLTK_DATA path
+os.environ['NLTK_DATA'] = "C:\\Users\\thura\\NLTK_data"
+
+
+
+
+
