f98ee9575acf9c5ed3c10efb06dba9172d26204a
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-15 20:44:11 +0300

    Fixed Java installation in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Install Java 11 (required for language_tool_python)
-apt-get update
-apt-get install -y openjdk-11-jre
 
-# Set JAVA_HOME environment variable
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# Install Java 11 (if not already installed)
+sudo apt update
+sudo apt install -y openjdk-11-jdk
+
+# Set JAVA_HOME
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Check if Java is installed
+java -version
 
 # Install Python dependencies
 pip install -r requirements.txt
