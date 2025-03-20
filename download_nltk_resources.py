8956a8e84e1f939e4bd05c71f8188bdc79e4fe08
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-20 13:31:53 +0300

    Ensure automatic NLTK data download

--- download_nltk_resources.py
+++ download_nltk_resources.py
@@ -1,18 +1,8 @@
 import nltk
-import os
 
-# Set NLTK_DATA path
-os.environ['NLTK_DATA'] = r"C:\Users\thura\QalamBot\nltk_data"
-print(f"NLTK resources will be downloaded to: {os.environ['NLTK_DATA']}")
-
-# Ensure the directory exists
-if not os.path.exists(os.environ['NLTK_DATA']):
-    os.makedirs(os.environ['NLTK_DATA'])
-
-# Download the necessary NLTK resources
-nltk.download('punkt', download_dir=os.environ['NLTK_DATA'])
-nltk.download('stopwords', download_dir=os.environ['NLTK_DATA'])
-nltk.download('words', download_dir=os.environ['NLTK_DATA'])
-nltk.download('omw-1.4', download_dir=os.environ['NLTK_DATA'])
-nltk.download('wordnet', download_dir=os.environ['NLTK_DATA'])
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('omw-1.4')
 
