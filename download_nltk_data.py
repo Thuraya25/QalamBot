21772fa34bfde9a45d67e2b1f992f600613bb65c
Thuraya Mohammad Althumali <Thuraya.Althumali@gmail.com>
2025-03-20 14:54:49 +0300

    Prepare bot for deployment

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import nltk
+# Specify the path to your local nltk_data folder
+nltk.data.path.append('./nltk_data')
 
-# Download NLTK data (e.g., stopwords, punkt, etc.)
-nltk.download('stopwords')
-nltk.download('punkt')
+# Check if the required data is available
+try:
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('corpora/stopwords')
+    nltk.data.find('corpora/wordnet')
+    print("All NLTK resources are available locally!")
+except LookupError:
+    print("One or more NLTK resources are missing.")
+nltk.download('punkt', download_dir='C:/Users/thura/QalamBot/nltk_data')
+nltk.download('stopwords', download_dir='C:/Users/thura/QalamBot/nltk_data')
+nltk.download('wordnet', download_dir='C:/Users/thura/QalamBot/nltk_data')
 
-print("NLTK data downloaded successfully!")
+import nltk
 
+# Download required NLTK resources
+resources = ['punkt', 'stopwords', 'wordnet', 'averaged_perceptron_tagger']
+for resource in resources:
+    nltk.download(resource)
+
+print("NLTK resources downloaded successfully.")
